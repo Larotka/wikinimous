49396f043e116b39303011349f99e67f499dd8f1
darv melv <darvieuxmelvin@gmail.com>
2022-09-14 14:41:51 +0200

    article model and seed articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "creating 10 articles"
+10.times do
+  Article.create(title: Faker::Movie.title, content: Faker::Quote.famous_last_words)
+end
+puts "done"
